a1ee9acd12a923036b8f62bffbf09c567cfa5dd9
Jacek <j.krzywniak@wp.pl>
2023-11-22 23:09:26 +0100

    lab f

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -2,13 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Location;
+use App\Service\WeatherUtil;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\Location;
-use App\Repository\MeasurementRepository;
-use Symfony\Bridge\Doctrine\Attribute\MapEntity;
-
 
 class WeatherController extends AbstractController
 {
@@ -16,10 +15,10 @@ class WeatherController extends AbstractController
     public function city(
         #[MapEntity(mapping: ['country' => 'country', 'city' => 'city'])]
         Location $location,
-        MeasurementRepository $repository,
+        WeatherUtil $util,
     ): Response
     {
-        $measurements = $repository->findByLocation($location);
+        $measurements = $util->getWeatherForLocation($location);
 
         return $this->render('weather/city.html.twig', [
             'location' => $location,
